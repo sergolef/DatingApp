bb647228f6d6d57456cf3c812f3e07c0589fd0a1
sergolef <sergolef@gmail.com>
2022-11-02 09:12:15 +0200

    added dbcontext&first entity

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private DataContext _cotext;
+
+        public UsersController(DataContext _context)
+        {
+            this._cotext = _context;
+        }
+
+        // GET: /<controller>/
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            return await _cotext.Users.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetUser(int id)
+        {
+            return await _cotext.Users.FindAsync(id);
+        }
+    
+    }
+}
+
